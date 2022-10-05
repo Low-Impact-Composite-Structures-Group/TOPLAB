bc853491b84430fb1374a09ec8ba40565378b63d
Victor Poorte <victorpoorte@gmail.com>
2022-10-05 17:52:22 +0200

    Implement thermal resistances
    The code and the tests for the thermal resistances has been implemented.
    The code should be complete for now, in later stages this may require
    changes, though this is not expected.

---
+++ src/thermodynamics/thermal_resistances.py
@@ -0,0 +1,93 @@
+from abc import abstractmethod
+from dataclasses import dataclass
+from typing import Protocol
+
+
+@dataclass
+class ThermalResistance:
+    """Thermal resistance is used to compute the thermal resistance of
+    a heat transfer mode.
+
+    Args:
+        heat_transfer_coefficient (float): Heat transfer coefficient of
+        the heat transfer mode.
+        surface_area (float): Surface are along which the heat transfer
+        mode acts.
+    """
+    heat_transfer_coefficient: float
+    surface_area: float
+
+    @property
+    def resistance(self) -> float:
+        """Compute the value of the thermal resistance.
+
+        This also accounts for null values in the surface area or the 
+        heat transfer coefficient, which would lead to infinite 
+        resistance.
+
+        Returns:
+            float: Value of the thermal resistance
+        """
+        if (
+            self.heat_transfer_coefficient == 0
+            or self.surface_area == 0
+        ):
+            return float("inf")
+        return 1 / (self.heat_transfer_coefficient * self.surface_area)
+
+
+@dataclass
+class ResistanceCoupling(Protocol):
+    """Define coupled (Thermal) resistances. Can be used to compute the
+    equivalent resistance.
+    """
+
+    @abstractmethod
+    def compute_equivalent_resistance(
+        self, resistances: list[float]
+    ) -> float:
+        """Compute the equivalent resistance of the resistances.
+
+        Args:
+            resistances (List[float]): List with values of the single 
+            resistances.
+        """
+        pass
+
+
+class ParallelResistances(ResistanceCoupling):
+    """Define (Thermal) resistances coupled in parallel. Can be used to
+    compute the equivalent resistance.
+    """
+
+    def compute_equivalent_resistance(
+        self, resistances: list[float]
+    ) -> float:
+        den = 0
+        for resistance in resistances:
+            if resistance == 0:
+                return 0
+            den += 1 / resistance
+        return 1 / den
+
+
+class SeriesResistances(ResistanceCoupling):
+    """Define (Thermal) resistances coupled in series. Can be used to
+    compute the equivalent resistance.
+    """
+
+    def compute_equivalent_resistance(
+        self, resistances: list[float]
+    ) -> float:
+        return sum(resistances)
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
+
+# End
