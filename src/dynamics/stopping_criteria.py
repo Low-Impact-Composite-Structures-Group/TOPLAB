ee651bcfc55e3a65c032fbee0734a5a56a3f5baf
Victor Poorte <victorpoorte@gmail.com>
2022-11-10 11:17:56 +0100

    Implement tank fill stopping criteria
    Tank is full, empty and target fill have been reached stopping criteria
    have been implemented. These can aid during the fill and drain analysis
    of tanks.
    
    The TargetFillIsREached class may be need to split up in two seperate
    classes where a lower and an upper limit are used.

--- src/dynamics/stopping_criteria.py
+++ src/dynamics/stopping_criteria.py
@@ -1,6 +1,8 @@
 from abc import abstractmethod
 from typing import Protocol
 
+EMPTY_LIMIT = 0.01  # A lower limit to define when the tank is empty
+
 
 class InitialState(Protocol):
     pressure: float
@@ -18,10 +20,7 @@ class TargetState(Protocol):
 
 class FuelTankState(Protocol):
     pressure: float
-
-
-class TargetState(Protocol):
-    pressure: float
+    fill: float
 
 
 class StoppingCriterion(Protocol):
@@ -40,7 +39,31 @@ class MaxPressure(StoppingCriterion):
     def is_met(
         self, fuel_tank_state: FuelTankState, target_state: TargetState
     ) -> bool:
-        return fuel_tank_state.pressure >= target_state.pressure
+        return fuel_tank_state.pressure >= target_state.max_pressure
+
+
+class IsEmpty(StoppingCriterion):
+
+    def is_met(
+        self, fuel_tank_state: FuelTankState, _: TargetState
+    ) -> bool:
+        return fuel_tank_state.fill <= EMPTY_LIMIT
+
+
+class IsFull(StoppingCriterion):
+
+    def is_met(
+        self, fuel_tank_state: FuelTankState, target_state: TargetState
+    ) -> bool:
+        return fuel_tank_state.fill >= 1
+
+
+class TargetFillIsReached(StoppingCriterion):
+
+    def is_met(
+        self, fuel_tank_state: FuelTankState, target_state: TargetState
+    ) -> bool:
+        return fuel_tank_state.fill >= target_state.fill
 
 
 def main():
