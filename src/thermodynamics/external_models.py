f1de9ccaf8ce461a8a8f9ea8f95d08764ccf9799
Victor Poorte <victorpoorte@gmail.com>
2022-11-11 11:30:04 +0100

    Include NaturalConvectionModel
    The natural convection model has been included. This is the model as
    suggested by Rompokos.

--- src/thermodynamics/external_models.py
+++ src/thermodynamics/external_models.py
@@ -4,7 +4,7 @@
 from abc import abstractmethod
 from typing import Protocol
 
-from src.thermodynamics.heat_transfer_modes import ForcedConvection
+from src.thermodynamics.heat_transfer_modes import ForcedConvection, NaturalCylinderConvection, NaturalSphereConvection
 from src.thermodynamics.thermal_resistances import (SeriesResistances,
                                                     ThermalResistance)
 
@@ -12,6 +12,7 @@ from src.thermodynamics.thermal_resistances import (SeriesResistances,
 class FuelTank(Protocol):
     characteristic_length: float
     exposed_surface: float
+    surface_area: float
 
 
 class Ambient(Protocol):
@@ -69,6 +70,35 @@ class ForcedConvectionModel(ExternalModel):
             tank.exposed_surface
         )
         return [exposed_resistance]
+
+
+class NaturalConvectionModel(ExternalModel):
+
+    def get_thermal_resistances(
+        self, 
+        tank: FuelTank, 
+        mission_section: MissionSection, 
+        surface_temperature: float
+    ) -> list[ThermalResistance]:
+        cylinder_convection = NaturalCylinderConvection(
+            mission_section.ambient,
+            tank.characteristic_length,
+            surface_temperature
+        )
+        cylinder_convection = ThermalResistance(
+            cylinder_convection.heat_transfer_coefficient,
+            tank.exposed_surface
+        )
+        spheres_convection = NaturalSphereConvection(
+            mission_section.ambient,
+            tank.characteristic_length,
+            surface_temperature
+        )
+        spheres_convection = ThermalResistance(
+            cylinder_convection.heat_transfer_coefficient,
+            tank.surface_area - tank.exposed_surface
+        )
+        return [cylinder_convection, spheres_convection]
 
 
 def main():
