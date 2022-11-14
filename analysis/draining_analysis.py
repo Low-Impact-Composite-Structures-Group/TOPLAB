a027996160766a745db4422b15483ee1179a1726
Victor Poorte <victorpoorte@gmail.com>
2022-11-14 15:02:22 +0100

    Include gas draining in the analysis
    The functionality of gas drining has been tested. This is to enable the
    analysis of this state in the tank. This is done by creating a gas
    analysis function, which is to be used to define the tank loading.

--- analysis/draining_analysis.py
+++ analysis/draining_analysis.py
@@ -5,7 +5,7 @@ from typing import Protocol
 
 from src.dynamics.dynamic_analysis import AnalyseMissionSection
 from src.dynamics.dynamic_models import DynamicModelFactory
-from src.dynamics.stopping_criteria import TankIsEmpty
+from src.dynamics.stopping_criteria import LowerPressureReached, TankIsEmpty
 from src.mission.mission_sections import MissionSection
 from src.multistep_methods.linear_multistep_methods import EulerMethod
 from src.thermodynamics.external_models import ForcedConvectionModel
@@ -29,23 +29,26 @@ class FuelFlow(Protocol):
     ...
 
 
+class TankState(Protocol):
+    ...
 
 @dataclass
 class TargetState:
-    pressure: float
+    max_pressure: float
+    min_pressure: float
     temperature: float
     fill: float
     mass: float
 
 
-def perform_draining_analysis(
+def liquid_draining_analysis(
     tank: FuelTank,
     initial_state: InitialState,
     fuel_flow: FuelFlow,
     insulation: Insulation,
     timestep: float = 60,
     heat_flux_factor: float = 1
-):
+) -> list[TankState]:
 
     # Assumptions about the mission section
     duration = 60e6
@@ -63,7 +66,8 @@ def perform_draining_analysis(
 
     # Define the target conditions
     target_conditions = TargetState(
-        pressure=None,
+        max_pressure=None,
+        min_pressure=None,
         temperature=None,
         fill=None,
         mass=None
@@ -86,6 +90,56 @@ def perform_draining_analysis(
     )
     return analysis.analyse_mission_section()
 
+
+def gas_draining_analysis(
+    tank: FuelTank,
+    initial_state: InitialState,
+    fuel_flow: FuelFlow,
+    insulation: Insulation,
+    min_pressure: float,
+    timestep: float = 60,
+    heat_flux_factor: float = 1
+) -> list[TankState]:
+
+    # Assumptions about the mission section
+    duration = 60e6
+    altitude = 10e3
+    mach_number = 0.85
+    mission_section = MissionSection(
+        duration,
+        [fuel_flow],
+        altitude,
+        mach_number
+    )
+
+    # Define the stopping criteria for the fuel tank
+    stopping_criteria = [LowerPressureReached()]
+
+    target_conditions = TargetState(
+        max_pressure=None,
+        min_pressure=min_pressure,
+        temperature=None,
+        fill=None,
+        mass=None
+    )
+
+    analysis = AnalyseMissionSection(
+        tank,
+        initial_state,
+        mission_section,
+        stopping_criteria,
+        target_conditions,
+        EulerMethod(timestep),
+        DynamicModelFactory(),
+        ThermodynamicModel(
+            SingleZoneModel(),
+            ForcedConvectionModel(),
+            insulation
+        ),
+        heat_flux_factor=heat_flux_factor
+    )
+    
+    return analysis.analyse_mission_section()
 
 
 def main():
