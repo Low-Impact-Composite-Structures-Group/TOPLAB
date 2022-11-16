8dd0a0af59a2af3db7895d722a7ea4a2be99f0d9
Victor Poorte <victorpoorte@gmail.com>
2022-11-16 13:33:54 +0100

    Working draining analysis

--- main.py
+++ main.py
@@ -1,113 +1,107 @@
 
 
-from analysis.draining_analysis import gas_draining_analysis, liquid_draining_analysis
 from analysis.recreate_rompokos_mission import perform_analysis
-from plotting.plot_tank_states import plot_tank_fill, plot_tank_loads, plot_tank_temperatures
-from src.insulation.foam_insulations import ConstantFoamInsulation
-from src.mission.mission_sections import OutFlow
-from src.tank_design.tank_shapes import CylindricalTankSphericalCaps
-from src.thermodynamics.tank_states import InitialState
 
 
-def test_gas_draining():
+# def test_gas_draining():
 
-    # Define tank
-    tank = CylindricalTankSphericalCaps.rompokos()
+#     # Define tank
+#     tank = CylindricalTankSphericalCaps.rompokos()
     
-    # Define insulation
-    thickness = 4e-2
-    insulation = ConstantFoamInsulation.polyvinylchloride(thickness)
+#     # Define insulation
+#     thickness = 4e-2
+#     insulation = ConstantFoamInsulation.polyvinylchloride(thickness)
 
-    # Define fuel flow
-    fuel_flow = OutFlow.rompokos_cruise("gas")
+#     # Define fuel flow
+#     fuel_flow = OutFlow.rompokos_cruise("gas")
 
-    # Define operating pressure window of the vessel
-    initial_pressures = [100e5, 200e5, 300e5, 400e5, 500e5, 600e5]
-    min_pressure = 10e5
+#     # Define operating pressure window of the vessel
+#     initial_pressures = [100e5, 200e5, 300e5, 400e5, 500e5, 600e5]
+#     min_pressure = 10e5
 
-    data = list()
-    for initial_pressure in initial_pressures:
-        # Define initial state of tank
-        initial_temperature = 100
-        initial_fill = 0.0
-        initial_state = InitialState(
-            initial_pressure, initial_temperature, initial_fill
-        )
+#     data = list()
+#     for initial_pressure in initial_pressures:
+#         # Define initial state of tank
+#         initial_temperature = 100
+#         initial_fill = 0.0
+#         initial_state = InitialState(
+#             initial_pressure, initial_temperature, initial_fill
+#         )
 
-        tank_states = gas_draining_analysis(
-            tank,
-            initial_state,
-            fuel_flow,
-            insulation,
-            min_pressure
-        )
-        data.append(tank_states)
+#         tank_states = gas_draining_analysis(
+#             tank,
+#             initial_state,
+#             fuel_flow,
+#             insulation,
+#             min_pressure
+#         )
+#         data.append(tank_states)
 
-    fig1 = plot_tank_loads(
-        data,
-        [f'{int(pressure * 1e-5)} bar' for pressure in initial_pressures]
-    )
-    fig2 = plot_tank_temperatures(
-        data,
-        [f'{int(pressure * 1e-5)} bar' for pressure in initial_pressures]
-    )
+#     fig1 = plot_tank_loads(
+#         data,
+#         [f'{int(pressure * 1e-5)} bar' for pressure in initial_pressures]
+#     )
+#     fig2 = plot_tank_temperatures(
+#         data,
+#         [f'{int(pressure * 1e-5)} bar' for pressure in initial_pressures]
+#     )
 
-    fig3 = plot_tank_fill(
-        tank_states, 60
-    )
+#     fig3 = plot_tank_fill(
+#         tank_states, 60
+#     )
 
-    fig1.show()
+#     fig1.show()
 
 
-def test_liquid_draining():
+# def test_liquid_draining():
 
-    # Define tank
-    tank = CylindricalTankSphericalCaps.rompokos()
+#     # Define tank
+#     tank = CylindricalTankSphericalCaps.rompokos()
     
-    # Define insulation
-    thickness = 4e-2
-    insulation = ConstantFoamInsulation.polyvinylchloride(thickness)
+#     # Define insulation
+#     thickness = 4e-2
+#     insulation = ConstantFoamInsulation.polyvinylchloride(thickness)
 
-    # Define fuel flow
-    fuel_flow = OutFlow.rompokos_cruise("liquid")
+#     # Define fuel flow
+#     fuel_flow = OutFlow.rompokos_cruise("liquid")
 
-    initial_pressures = [1.5e5, 2.0e5, 2.5e5, 3.0e5]
+#     initial_pressures = [1.5e5, 2.0e5, 2.5e5, 3.0e5]
 
-    data = list()
-    for initial_pressure in initial_pressures:
-        # Define initial state of tank
-        initial_temperature = None
-        initial_fill = 0.95
-        initial_state = InitialState(
-            initial_pressure, initial_temperature, initial_fill
-        )
+#     data = list()
+#     for initial_pressure in initial_pressures:
+#         # Define initial state of tank
+#         initial_temperature = None
+#         initial_fill = 0.95
+#         initial_state = InitialState(
+#             initial_pressure, initial_temperature, initial_fill
+#         )
 
-        tank_states = liquid_draining_analysis(
-            tank,
-            initial_state,
-            fuel_flow,
-            insulation
-        )
-        data.append(tank_states)
-        print(max(tank_states, key=lambda x: x.pressure))
-    fig1 = plot_tank_loads(
-        data,
-        [f'{int(pressure * 1e-5)} bar' for pressure in initial_pressures]
-    )
-    fig2 = plot_tank_temperatures(
-        data,
-        [f'{int(pressure * 1e-5)} bar' for pressure in initial_pressures]
-    )
+#         tank_states = liquid_draining_analysis(
+#             tank,
+#             initial_state,
+#             fuel_flow,
+#             insulation
+#         )
+#         data.append(tank_states)
+#         print(max(tank_states, key=lambda x: x.pressure))
+#     fig1 = plot_tank_loads(
+#         data,
+#         [f'{int(pressure * 1e-5)} bar' for pressure in initial_pressures]
+#     )
+#     fig2 = plot_tank_temperatures(
+#         data,
+#         [f'{int(pressure * 1e-5)} bar' for pressure in initial_pressures]
+#     )
 
-    fig3 = plot_tank_fill(
-        tank_states, 60
-    )
+#     fig3 = plot_tank_fill(
+#         tank_states, 60
+#     )
 
-    fig1.show()
+#     fig1.show()
 
 
 def main():
-    pass
+    perform_analysis()
 
 
 if __name__ == "__main__":
