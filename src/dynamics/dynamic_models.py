8a5c30a4894fcaa74416c664d656e2381d834436
Victor Poorte <victorpoorte@gmail.com>
2022-11-17 16:02:20 +0100

    Split model factories per fuel phase
    The dynamic model factory has been split in two seperate factories, one
    for twopahse analysis and one for two phase analysis.

--- src/dynamics/dynamic_models.py
+++ src/dynamics/dynamic_models.py
@@ -610,10 +610,48 @@ class DynamicModelFactory:
         target_conditions: TargetConditions
     ) -> DynamicModel:
         if tank_state.phase == "twophase":
-            if target_conditions.min_pressure is not None:
-                if tank_state.pressure <= target_conditions.min_pressure:
-                    return TwoPhaseLimitLowerPressureModel
+            return TwoPhaseFactory().get_dynamic_model(
+                tank_state, target_conditions
+            )
+        return SinglePhaseFactory().get_dynamic_model(
+            tank_state, target_conditions
+        )
+
+
+class TwoPhaseFactory:
+
+    def get_dynamic_model(
+        self,
+        tank_state: TankState,
+        target_conditions: TargetConditions
+    ) -> DynamicModel:
+        if (
+            target_conditions.min_pressure is None
+            and target_conditions.max_pressure is None
+        ):
             return TwoPhaseModel
+        if tank_state.pressure <= target_conditions.min_pressure:
+            return TwoPhaseLimitLowerPressureModel
+        return TwoPhaseModel
+
+
+class SinglePhaseFactory:
+
+    def get_dynamic_model(
+        self,
+        tank_state: TankState,
+        target_conditions: TargetConditions
+    ) -> DynamicModel:
+        if (
+            target_conditions.min_pressure is None
+            and target_conditions.max_pressure is None
+        ):
+            return SinglePhaseModel
+        if (
+            target_conditions.min_pressure is not None
+            and target_conditions.min_pressure >= tank_state.pressure
+        ):
+            return SinglePhaseLimitLowerPressureModel
         return SinglePhaseModel
 
 
