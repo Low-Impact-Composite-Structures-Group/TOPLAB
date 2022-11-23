7e8d8ee8955c4408e5e8bce70e6cb5835ed4fd39
Victor Poorte <victorpoorte@gmail.com>
2022-11-23 12:09:29 +0100

    Implement filling facade

--- analysis/recreate_ahluwalia_fill_analysis.py
+++ analysis/recreate_ahluwalia_fill_analysis.py
@@ -1,29 +1,18 @@
 
 
-from typing import Protocol
-
 import matplotlib.pyplot as plt
 
+from facades.analysis_facades import (FillingAnalysisFacade, InitialConditions,
+                                      OperatingEnvelope, TankDimensions,
+                                      TargetConditions)
 from plotting.plot_tank_states import (plot_tank_fill,
                                        plot_thermo_mechanical_loading)
-from src.dynamics.dynamic_analysis import AnalyseMissionSection
-from src.dynamics.dynamic_models import DynamicModelFactory
-from src.dynamics.stopping_criteria import TankIsFull
 from src.fluids.hydrogen_retrievers import SinglePhaseRequester
 from src.insulation.foam_insulations import ConstantFoamInsulation
+from src.materials.materials import Composite
+from src.mission.mission import Mission
 from src.mission.mission_sections import InFlow, MissionSection
-from src.multistep_methods.linear_multistep_methods import EulerMethod
 from src.tank_design.tank_shapes import CylindricalTankSphericalCaps
-from src.thermodynamics.external_models import ForcedConvectionModel
-from src.thermodynamics.internal_models import SingleZoneModel
-from src.thermodynamics.tank_states import InitialState, TargetState
-from src.thermodynamics.thermodynamic_models import ThermodynamicModel
-
-
-class TankState(Protocol):
-    pressure: float
-    temperature: float
-    fill: float
 
 
 def perform_analysis():
@@ -32,24 +21,32 @@ def perform_analysis():
     pressure = 8e5
     temperature = 50
     fill = 0.0
-    initial_conditions = InitialState(
+    initial_conditions = InitialConditions(
         pressure, temperature, fill
     )
 
     # Define the fuel tank
     tank = CylindricalTankSphericalCaps.ahluwalia()
+    tank_radius = tank.radius
+    tank_body = tank.body_length
+    tank_dimensions = TankDimensions(tank_radius, tank_body)
 
-    # Define the stopping criteria for the fuel tank
-    stopping_criteria = [
-        TankIsFull()
-    ]
+    # Define the material of the tank
+    winding_angle = 55
+    material = Composite.carbon(winding_angle)
 
     # Define the target conditions
-    target_conditions = TargetState(
-        pressure=10e5,
-        temperature=None,
-        fill=1.0,
-        mass=11.0
+    target_conditions = TargetConditions(
+        fill=0.97,
+        fuel_mass=11.0
+    )
+
+    # Define the operating envelope of the fuel tank
+    max_pressure = 10e5
+    min_pressure = None
+    min_temperature = None
+    operating_envelope = OperatingEnvelope(
+        max_pressure, min_pressure, min_temperature
     )
 
     # Define insulation and thermodynamic model
@@ -57,22 +54,6 @@ def perform_analysis():
     insulation = ConstantFoamInsulation.rohacell(
         insulation_thickens
     )
-    thermodynamic_model = ThermodynamicModel(
-        SingleZoneModel(),
-        ForcedConvectionModel(),
-        insulation
-    )
-
-    # Define the dynamic model factory
-    dynamic_model_factory = DynamicModelFactory()
-
-    # Define the heat flux factory, which is to be used to account for
-    # extra losses
-    heat_flux_factor = 1
-
-    # Time integration and steps
-    timestep = 1
-    multistep_method = EulerMethod(timestep)
 
     duration = 1e4
     mass_flow = 0.01
@@ -91,33 +72,25 @@ def perform_analysis():
             altitude,
             mach_number
         )
+    mission = Mission([mission_section])
 
-    analysis = AnalyseMissionSection(
-        tank,
+    tank_performance = FillingAnalysisFacade.analyse(
+        tank_dimensions,
+        material,
+        insulation,
+        mission,
         initial_conditions,
-        mission_section,
-        stopping_criteria,
-        target_conditions,
-        multistep_method,
-        dynamic_model_factory,
-        thermodynamic_model,
-        heat_flux_factor=heat_flux_factor
+        operating_envelope,
+        target_conditions
     )
 
-    tank_states = analysis.analyse_mission_section()
-
-    initial_conditions = InitialState(
-        tank_states[-1].pressure,
-        tank_states[-1].temperature,
-        tank_states[-1].fill 
-    )
+    tank_states = tank_performance.tank_states
 
     y1ticks = None
     y2ticks = None
     xticks = None
     plot_thermo_mechanical_loading(
         tank_states,
-        timestep,
         xticks,
         y1ticks,
         y2ticks
@@ -126,7 +99,6 @@ def perform_analysis():
     y2ticks = None
     plot_tank_fill(
         tank_states,
-        timestep,
         xticks,
         y1ticks,
         y2ticks
@@ -135,7 +107,7 @@ def perform_analysis():
 
 
 def main():
-    pass
+    perform_analysis()
 
 
 if __name__ == "__main__":
--- facades/analysis_facades.py
+++ facades/analysis_facades.py
@@ -5,7 +5,8 @@ from typing import Protocol
 from src.dynamics.dynamic_analysis import MissionAnalysis
 from src.dynamics.dynamic_models import DynamicModelFactory
 from src.dynamics.stopping_criteria import (NoFuelMass, StoppingCriterion,
-                                            TankIsEmpty)
+                                            TankIsEmpty, TargetFillReached,
+                                            TargetMassReached)
 from src.efficiencies.tank_performance import TankPerformance
 from src.mission.mission import Mission
 from src.mission.mission_sections import MissionSection
@@ -15,7 +16,6 @@ from src.thermodynamics.external_models import ForcedConvectionModel
 from src.thermodynamics.internal_models import SingleZoneModel
 from src.thermodynamics.tank_states import InitialState, TargetState
 from src.thermodynamics.thermodynamic_models import ThermodynamicModel
-
 
 # The lower mass limit is to be used for draining analysis og gas tanks
 LOWER_MASS_LIMIT = 500
@@ -56,6 +56,12 @@ class InitialConditions:
     pressure: float
     temperature: float
     fill: float
+
+
+@dataclass
+class TargetConditions:
+    fuel_mass: float
+    fill: float 
 
 
 class AnalysisFacade(Protocol):
@@ -119,6 +125,7 @@ class AnalysisFacade(Protocol):
             initial_conditions.temperature,
             initial_conditions.fill
         )
+
 
 class DrainingAnalysisFacade(AnalysisFacade):
     
@@ -199,6 +206,59 @@ class MissionAnalysisFacade(AnalysisFacade):
     @classmethod
     def _define_stopping_criteria(cls) -> list[StoppingCriterion]:
         return list()
+
+
+class FillingAnalysisFacade(AnalysisFacade):
+
+    @classmethod
+    def analyse(
+        cls,
+        tank_dimensions: TankDimensions,
+        material: Material,
+        insulation: Insulation,
+        mission: Mission,
+        initial_conditions: InitialConditions,
+        operating_envelope: OperatingEnvelope,
+        target_conditions: TargetConditions,
+    ) -> TankPerformance:
+        print("Temporary fix, set timestep to 1")
+        MULTISTEP_METHOD.timestep = 1
+        initial_state = cls._define_initial_state(initial_conditions)
+        tank = cls._define_tank(
+        tank_dimensions, material, operating_envelope, initial_state 
+        )
+        tank_states = MissionAnalysis.perform_analysis(
+            tank,
+            initial_state,
+            mission,
+            cls._define_stopping_criteria(),
+            cls._define_target_conditions(
+                operating_envelope, target_conditions
+            ),
+            MULTISTEP_METHOD,
+            DYNAMIC_MODEL_FACTORY,
+            cls._define_thermal_model(insulation),
+            HEAT_FLUX_FACTOR
+        )
+        return TankPerformance(tank, insulation, tank_states)
+
+    @classmethod
+    def _define_stopping_criteria(cls) -> list[StoppingCriterion]:
+        return [TargetFillReached(), TargetMassReached()]
+
+    @staticmethod
+    def _define_target_conditions(
+        operating_envelope: OperatingEnvelope,
+        target_conditions: TargetConditions
+    ) -> TargetState:
+        target_conditions = TargetState(
+            operating_envelope.max_pressure,
+            operating_envelope.min_pressure,
+            operating_envelope.min_temperature,
+            target_conditions.fill,
+            target_conditions.fuel_mass
+        )
+        return target_conditions
 
 
 def main():
