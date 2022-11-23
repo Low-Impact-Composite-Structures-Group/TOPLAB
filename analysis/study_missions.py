641292f7fd01a4ad3003881c95f297ba1ac8a176
Victor Poorte <victorpoorte@gmail.com>
2022-11-23 11:01:43 +0100

    Refactor to use the analysis facade

--- analysis/study_missions.py
+++ analysis/study_missions.py
@@ -1,15 +1,13 @@
-from plotting.plot_tank_states import plot_tank_fill, plot_tank_loads, plot_tank_temperatures
-from src.thermodynamics.internal_models import SingleZoneModel
-from src.thermodynamics.external_models import ForcedConvectionModel
-from src.thermodynamics.thermodynamic_models import ThermodynamicModel
-from src.dynamics.dynamic_models import DynamicModelFactory
-from src.mission.mission import Mission
-from src.multistep_methods.linear_multistep_methods import EulerMethod
-from src.tank_design.tank_shapes import CylindricalTankSphericalCaps
-from src.materials.materials import Metal
-from src.thermodynamics.tank_states import InitialState, TargetState
-from src.dynamics.dynamic_analysis import MissionAnalysis
+from plotting.plot_tank_states import (plot_tank_fill, plot_tank_loads,
+                                       plot_tank_temperatures)
+from src.facades.analysis_facades import (MissionAnalysisFacade,
+                                          OperatingEnvelope, TankDimensions)
 from src.insulation.foam_insulations import ConstantFoamInsulation
+from src.materials.materials import Composite
+from src.mission.mission import Mission
+from src.tank_design.tank_shapes import CylindricalTankSphericalCaps
+from src.thermodynamics.tank_states import InitialState
+
 
 def perform_analysis():
 
@@ -30,8 +28,13 @@ def perform_analysis():
     fill = 0.97
     initial_state = InitialState(pressure, temperature, fill)
 
-    # Define minimum pressure in the case og gas phase draining
+    # Define operating window of the pressure vessel
     min_pressure = 1.1e5
+    operating_window = OperatingEnvelope(
+        max_pressure=None,
+        min_pressure=min_pressure,
+        min_temperature=None
+    )
 
     # Define required fuel
     fuel_masses = [mission.required_fuel for mission in missions]
@@ -41,7 +44,7 @@ def perform_analysis():
         for fuel_mass in fuel_masses
     ]
 
-    # Define tank
+    # Define tank dimensions, based on the required fuel volume
     VOLUME_MARGIN = 1.15
     tank_volumes = [
         VOLUME_MARGIN * fuel_volume for fuel_volume in fuel_volumes
@@ -53,47 +56,30 @@ def perform_analysis():
         )
         for tank_volume in tank_volumes
     ]
-    total_tank_lengths = [
-        tank_length + 2 * tank_radius
-        for tank_length in tank_lengths
-    ]
-    material = Metal.aluminum()
-    tanks = [
-        CylindricalTankSphericalCaps(
-            tank_radius, total_tank_length, material, pressure
-        )
-        for total_tank_length in total_tank_lengths
+    tanks_dimensions = [
+        TankDimensions(tank_radius, body_length)
+        for body_length in tank_lengths
     ]
 
     # Things required for the analysis
     insulation_thickness = 8e-2
     insulation = ConstantFoamInsulation.rohacell(insulation_thickness)
-    stopping_criteria = []
-    target_conditions = TargetState(
-        None, min_pressure, None, None, None
-    )
-    timestep = 60
-    multistep_method = EulerMethod(timestep)
-    dynamic_model_factory = DynamicModelFactory()
-    thermal_model = ThermodynamicModel(
-        SingleZoneModel(), ForcedConvectionModel(), insulation
-    )
-    heat_flux_factor = 1
+
+    # Define the material of the tank
+    winding_angle = 55
+    material = Composite.carbon(winding_angle)
 
     # Analyse mission
     data = [
-        MissionAnalysis.perform_analysis(
-            tank,
-            initial_state,
+        MissionAnalysisFacade.analyse(
+            tank_dimension,
+            material,
+            insulation,
             mission,
-            stopping_criteria,
-            target_conditions,
-            multistep_method,
-            dynamic_model_factory,
-            thermal_model,
-            heat_flux_factor
+            initial_state,
+            operating_window
         )
-        for tank, mission in zip(tanks, missions)
+        for tank_dimension, mission in zip(tanks_dimensions, missions)
     ]
 
     time_ticks = list(range(0, 13, 2))
@@ -102,18 +88,26 @@ def perform_analysis():
     fill_ticks = [i/10 for i in range(0, 11, 2)]
     temp_ticks = [i/10 for i in range(215, 251, 5)]
     fig = plot_tank_loads(
-        data, labels, time_ticks, pressure_ticks
+        [row.tank_states for row in data],
+        labels,
+        time_ticks,
+        pressure_ticks
     )
     plot_tank_temperatures(
-        data, labels, time_ticks, temp_ticks
+        [row.tank_states for row in data],
+        labels,
+        time_ticks,
+        temp_ticks
     )
-    plot_tank_fill(data[-1], time_ticks, mass_ticks, fill_ticks)
+    plot_tank_fill(
+        data[-1].tank_states, time_ticks, mass_ticks, fill_ticks
+    )
     fig.show()
 
 
 
 def main():
-    pass
+    perform_analysis()
 
 
 if __name__ == "__main__":
