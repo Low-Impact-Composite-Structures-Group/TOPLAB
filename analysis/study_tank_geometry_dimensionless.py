f443703ecaa54aeabd4c59ca3df84032a2c13666
Dante Raso <d.raso@tudelft.nl>
2024-10-09 16:20:00 +0200

    branching off from main
    The initial branching from VP's original code by DR. The goal of DR is to build upon the code of VP to eventually include the following features:
    - a geometric optimization for a single tank. The handles will be the three dimensionless parameters introduced by Winnefeld et al. in their 2018 paper, with the objective being max(grav_eff)
    - optimizing the insulation thickness, again with the objective of maximizing grav. eff.
    - integrating heat load from external HXs or other sources of heat leakage to the tank via Ahluwalia 2008 model.
    - introduce multi-tank/state framework whereby the optimal multi-tank layout is an output of the study.
    
    The addition of study_tank_geometry_dimensionless.py is the first step towards accomplishing the first feature listed above. It was copied from study_tank_geometry_radius, originally authored by VP.
    
    This project is part of DR's PhD research in collaboration with TRIATHLON.

---
+++ analysis/study_tank_geometry_dimensionless.py
@@ -0,0 +1,85 @@
+
+
+from plotting.plot_tank_states import (plot_tank_efficiencies, plot_tank_fill,
+                                       plot_tank_loads, plot_tank_temperatures)
+from facades.analysis_facades import (DrainingAnalysisFacade, InitialConditions,
+                                          OperatingEnvelope, TankDimensions)
+from src.insulation.foam_insulations import ConstantFoamInsulation
+from src.materials.materials import Metal
+from src.mission.mission_sections import OutFlow
+
+
+def perform_analysis():
+
+    # Define the initial state of the tank
+    pressure = 140e3
+    temperature = None
+    fill = 0.97
+    initial_state = InitialConditions(
+        pressure, temperature, fill
+    )
+
+    # Define insulation and thermal model
+    thickness = 4e-2
+    insulation = ConstantFoamInsulation.rohacell(thickness)
+
+    # Define tank material
+    tank_material = Metal.aluminum()
+
+    # Define fuel flow
+    fuel_flow = OutFlow.rompokos_cruise("liquid")
+
+    # Define the minimum pressure of the tank
+    min_pressure = 1.3e5
+
+    # Define tank dimensions
+    body_length = 5
+    radii = [i / 100 for i in range(25, 276, 25)]
+    labels = [f'{radius} m' for radius in radii]
+
+    # Perform the analysis
+    performances = [
+        DrainingAnalysisFacade.analyse(
+            TankDimensions(
+                radius, body_length
+            ),
+            tank_material,
+            insulation,
+            fuel_flow.mass_flow,
+            fuel_flow.phase,
+            initial_state,
+            OperatingEnvelope(
+                None,
+                min_pressure,
+                None
+            )
+        )
+        for radius in radii
+    ]
+    data = [performance.tank_states for performance in performances]
+
+    xticks = list(range(0, 31, 5))
+    yticks = list(range(0, 11, 2))
+    fig1 = plot_tank_loads(data, labels, xticks, yticks)
+    yticks = list(range(20, 33, 2))
+    fig2 = plot_tank_temperatures(data, labels, xticks, yticks)
+    y1ticks = list(range(0, 12001, 2000))
+    y2ticks = [i / 10 for i in range(0, 11, 2)]
+    fig3 = plot_tank_fill(data[-1], xticks, y1ticks, y2ticks)
+    xticks = [i / 100 for i in range(0, 301, 50)]
+    yticks = [i / 100 for i in range(70, 101, 5)]
+    fig4 = plot_tank_efficiencies(
+        performances, radii, "Radius [m]", xticks, yticks
+    )
+    fig1.show()
+
+
+def main():
+    perform_analysis()
+
+
+if __name__ == "__main__":
+    main()
+
+
+# End
