2bbe26e3cc3cb3985c0cba7ccee47e47037a5b0b
Dante Raso <d.raso@tudelft.nl>
2024-11-29 14:12:01 +0100

    Update input parameters and configure automatic saving of results

--- optimization/radius_vs_insulation/optimization_radius_vs_insulation.py
+++ optimization/radius_vs_insulation/optimization_radius_vs_insulation.py
@@ -17,6 +17,7 @@ from mpl_toolkits.mplot3d import Axes3D
 import yaml
 import os
 import json
+import pickle
 
 
 def perform_analysis():
@@ -38,6 +39,7 @@ def perform_analysis():
     thickness_min = config.get('minimum insulation thickness', None)
     thickness_max = config.get('maximum insulation thickness', None)
     VOLUME_MARGIN = config.get('volume margin', None)
+    psi = config.get('psi', None)
     tank_material_type = config.get('tank material type', None)
     tank_material_name = config.get('tank material', None)
     winding_angle_degrees = config.get('winding angle', None)
@@ -46,6 +48,7 @@ def perform_analysis():
     SAVE_RESULTS = config.get('save results', None)
     PLOT_EXTRA = config.get('plot extra', None)
     RESPONSE_SURFACE = config.get('response surface', None)
+    grid_size = config.get('response surface grid size', None)
     RENDER_3D = config.get('render 3D', None)
     min_pressure = config.get('operating floor pressure', None)
     min_temperature = config.get('operating floor temperature', None)
@@ -102,16 +105,18 @@ def perform_analysis():
     all_tank_states = []
     all_thicknesses = []
     all_radii = []
+    all_graveffs = []
+    all_vol_effs = []
 
     # Define the objective function
     def objective_function(params):
         radius, thickness = params
-        length = WinnefeldTank.length_from_radius_b_and_volume(radius, VOLUME_MARGIN * fuel_volume, 0.5*radius)
+        length = WinnefeldTank.length_from_radius_b_and_volume(radius, VOLUME_MARGIN * fuel_volume, radius/psi)
         insulation = ConstantFoamInsulation.rohacell(thickness)
         print(f"Analyzing radius: {radius:.4f}, thickness: {thickness:.4f}")
         performance = MissionAnalysisFacade.analyse(
             GenericTankDimensions(
-                radius, WinnefeldTank.length_from_radius_b_and_volume(radius, VOLUME_MARGIN * fuel_volume, 0.5*radius), radius, 0.5*radius),
+                radius, WinnefeldTank.length_from_radius_b_and_volume(radius, VOLUME_MARGIN * fuel_volume, radius/psi), radius, radius/psi),
             tank_material,
             insulation,
             mission,
@@ -122,27 +127,29 @@ def perform_analysis():
         all_tank_states.append(performance.tank_states)
         all_thicknesses.append(thickness)
         all_radii.append(radius)
-        print(f"Length = {length} m")
-        print(f"Gravimetric efficiency = {performance.gravimetric_efficiency}")
+        all_graveffs.append(performance.gravimetric_efficiency)
+        all_vol_effs.append(performance.volumetric_efficiency)
+        print(f"Length = {length:.4f} m")
+        print(f"Gravimetric efficiency = {performance.gravimetric_efficiency:.4f}")
         return -performance.gravimetric_efficiency
 
     # Perform the optimization
     result = minimize(objective_function, initial_guess, method='Nelder-Mead', bounds=bounds_radius_thickness)
     optimal_radius, optimal_thickness = result.x
-    optimal_length = WinnefeldTank.length_from_radius_b_and_volume(optimal_radius, VOLUME_MARGIN * fuel_volume, 0.5 * optimal_radius)
+    optimal_length = WinnefeldTank.length_from_radius_b_and_volume(optimal_radius, VOLUME_MARGIN * fuel_volume, optimal_radius/psi)
     print(f'Optimal radius: {optimal_radius:.4f} m')
-    print(f'Optimal b: {0.5 * optimal_radius:.4f} m')
+    print(f'Optimal b: {optimal_radius/psi:.4f} m')
     print(f'Optimal thickness: {optimal_thickness:.4f} m')
     print(f'Optimal length: {optimal_length:.4f} m')
     print(f'Minimized gravimetric efficiency: {-result.fun:.4f}')
 
     optimal_volumetric_efficiency = [performance.volumetric_efficiency for performance in all_performances]
-    print(f"Max volumetric efficiency = ", max(optimal_volumetric_efficiency))
+    print(f"Max volumetric efficiency = {max(optimal_volumetric_efficiency):.4f}")
 
     # Perform the analysis with the optimized parameters
     optimal_performance = [MissionAnalysisFacade.analyse(GenericTankDimensions(
             optimal_radius, WinnefeldTank.length_from_radius_b_and_volume(
-            optimal_radius, VOLUME_MARGIN * fuel_volume, 0.5 * optimal_radius), optimal_radius, 0.5 * optimal_radius),
+            optimal_radius, VOLUME_MARGIN * fuel_volume, optimal_radius/psi), optimal_radius, optimal_radius/psi),
         tank_material,
         ConstantFoamInsulation.rohacell(optimal_thickness),
         mission,
@@ -152,17 +159,16 @@ def perform_analysis():
     # Listify optimal tank performance
     optimum = [performance.tank_states for performance in optimal_performance]
 
-    # Compute zeta
+    # Compute zeta (zeta = r/t) for each radius and thickness
     zeta_values = [all_radii / all_thicknesses for all_radii, all_thicknesses in zip(all_radii, all_thicknesses)]
-    labels = [f'{zeta} [-]' for zeta in zeta_values]
+    labels = [f'{zeta} [m/m]' for zeta in zeta_values]
 
      # Create plots for tank loads and efficiencies searched in the optimization
     tank_loads_fig = plot_tank_loads(optimum, labels, None, None)
-    etas_fig = plot_tank_efficiencies_scatter(all_performances, zeta_values, "psi (c/b) [m/m]", None, None)
+    etas_fig = plot_tank_efficiencies_scatter(all_performances, zeta_values, "zeta (r/t) [m/m]", None, None)
 
 
     if RESPONSE_SURFACE:
-        grid_size = 10
         # Create a grid of radius and b values
         radius_values = np.linspace(radius_min, radius_max, grid_size)
         thickness_values = np.linspace(thickness_min, thickness_max, grid_size)
@@ -179,7 +185,7 @@ def perform_analysis():
                 print(f"Case {i * grid_size + j + 1} of {grid_size ** 2}")
                 performance = MissionAnalysisFacade.analyse(GenericTankDimensions(
                     radius, WinnefeldTank.length_from_radius_b_and_volume(
-                    radius, VOLUME_MARGIN * fuel_volume, 0.5*radius), radius, 0.5*radius),
+                    radius, VOLUME_MARGIN * fuel_volume, radius/psi), radius, radius/psi),
                     tank_material,
                     ConstantFoamInsulation.rohacell(thickness),
                     mission,
@@ -195,21 +201,21 @@ def perform_analysis():
         ax_ge.plot_surface(radius_grid, thickness_grid, gravimetric_efficiency_grid, cmap='viridis')
         ax_ge.plot([optimal_radius], [optimal_thickness], [-result.fun], marker='o', markersize=5, color='r')
 
-        ax_ge.set_xlabel('Radius')
-        ax_ge.set_ylabel('b')
+        ax_ge.set_xlabel('Radius [m]')
+        ax_ge.set_ylabel('Insulation thickness [m]')
         ax_ge.set_zlabel('Gravimetric Efficiency')
         ax_ge.set_title('Gravimetric Efficiency Response Surface')
 
-        ax_ge.text2D(0.05, 0.95, f"Optimal Radius = {optimal_radius:.2f}", transform=ax_ge.transAxes)
-        ax_ge.text2D(0.05, 0.90, f"Optimal thickness = {optimal_thickness:.2f}", transform=ax_ge.transAxes)
+        ax_ge.text2D(0.05, 0.95, f"Optimal Radius = {optimal_radius:.2f} m", transform=ax_ge.transAxes)
+        ax_ge.text2D(0.05, 0.90, f"Optimal thickness = {optimal_thickness:.2f} m", transform=ax_ge.transAxes)
         ax_ge.text2D(0.05, 0.85, f"Optimal Gravimetric Efficiency = {-result.fun:.2f}", transform=ax_ge.transAxes)
 
         fig_ve_surface = plt.figure()
         ax_ve = fig_ve_surface.add_subplot(111, projection='3d')
         ax_ve.plot_surface(radius_grid, thickness_grid, volumetric_efficiency_grid, cmap='viridis')
 
-        ax_ve.set_xlabel('Radius')
-        ax_ve.set_ylabel('b')
+        ax_ve.set_xlabel('Radius [m]')
+        ax_ve.set_ylabel('Insulation thickness [m]')
         ax_ve.set_zlabel('Volumetric Efficiency')
         ax_ve.set_title('Volumetric Efficiency Response Surface')
 
@@ -217,35 +223,9 @@ def perform_analysis():
         ax_ve.text2D(0.05, 0.90, f"Optimal thickness = {optimal_thickness:.2f}", transform=ax_ve.transAxes)
         ax_ve.text2D(0.05, 0.85, f"Optimal Volumetric Efficiency = {max(optimal_volumetric_efficiency):.2f}", transform=ax_ve.transAxes)
 
-     # Show the figures
-    tank_loads_fig.show()
-    etas_fig.show()
-
-    # Save results if the flag is enabled
-    if SAVE_RESULTS:
-        results_dir = 'results'
-        os.makedirs(results_dir, exist_ok=True)
-        results_file = os.path.join(results_dir, 'results.json')
-
-        results_data = {
-            'optimal_radius': optimal_radius,
-            'optimal_thickness': optimal_thickness,
-            'optimal_length': optimal_length,
-            'minimized_gravimetric_efficiency': -result.fun,
-            'max_volumetric_efficiency': max(optimal_volumetric_efficiency),
-            'zeta_values': zeta_values
-        }
-
-        with open(results_file, 'w') as file:
-            json.dump(results_data, file, indent=4)
-
-    # Record the end time
-    end_time = time.time()
-    print(f"Analysis completed in {end_time - start_time:.2f} seconds")
-
     # Optional 3D render
     if RENDER_3D:
-        plot_tank(optimal_radius, 0.5*optimal_radius, optimal_length)
+        tank_render = plot_tank(optimal_radius, optimal_radius/psi, optimal_length)
 
     # Optional plotting
     if PLOT_EXTRA:
@@ -260,6 +240,43 @@ def perform_analysis():
         plot_tank_fill(
             all_tank_states[-1]
         )
+
+    # Save results if the flag is enabled
+    if SAVE_RESULTS:
+        script_dir = os.path.dirname(__file__)
+        results_dir = os.path.join(script_dir, '..', 'results')
+        os.makedirs(results_dir, exist_ok=True)
+        results_file = os.path.join(results_dir, 'results.json')
+
+        results_data = {
+            'optimal_radius': optimal_radius,
+            'optimal_thickness': optimal_thickness,
+            'optimal_length': optimal_length,
+            'minimized_gravimetric_efficiency': -result.fun,
+            'max_volumetric_efficiency': max(optimal_volumetric_efficiency),
+            'zeta_values': zeta_values,
+            'volumetric_efficiencies': all_vol_effs,
+            'gravimetric_efficiencies': all_graveffs
+        }
+
+        tank_loads_fig.savefig(os.path.join(results_dir, "tank_loads_plot.png"), dpi=300, bbox_inches='tight')
+        etas_fig.savefig(os.path.join(results_dir, "etas_plot.png"), dpi=300, bbox_inches='tight')
+
+        with open(results_file, 'w') as file:
+            json.dump(results_data, file, indent=4)
+        with open(os.path.join(results_dir, 'fig_ge_surface.pkl'), 'wb') as file:
+            pickle.dump(fig_ge_surface, file)
+        with open(os.path.join(results_dir, 'fig_ve_surface.pkl'), 'wb') as file:
+            pickle.dump(fig_ve_surface, file)
+        with open(os.path.join(results_dir, 'tank_render.pkl'), 'wb') as file:
+            pickle.dump(tank_render, file)
+
+
+    # Record the end time
+    end_time = time.time()
+    print(f"Analysis completed in {end_time - start_time:.2f} seconds")
+
+
 
     # Show all plots at once
     plt.show()
