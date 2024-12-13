66dfc65c2cd164a1a92e26965bf402c21beb9ba9
Dante Raso <d.raso@tudelft.nl>
2024-12-13 10:41:04 +0100

    update TODO comment to specify using dependency injection for YAML input configuration

--- facades/analysis_facades.py
+++ facades/analysis_facades.py
@@ -28,7 +28,8 @@ LOWER_MASS_LIMIT = 500 # TODO: find suitable lower limit
 
 
 # Factories and constants to be used in the analysis
-#TODO: refactor to use yaml input for these constants
+#TODO: refactor to use yaml input for these constants using dependency injection
+# to pass the configuration to the classes and functions that need it
 TIMESTEP = 1000
 MULTISTEP_METHOD = EulerMethod(TIMESTEP)
 DYNAMIC_MODEL_FACTORY = DynamicModelFactory()
