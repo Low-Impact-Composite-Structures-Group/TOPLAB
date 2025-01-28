805f24d7863008307233e947d69fd70f5f91eeae
Dante Raso <d.raso@tudelft.nl>
2025-01-28 09:25:44 +0100

    getting things setup for the CcH2 mission analysis.
    Temporarily modelling the system with LH2 for the development of the support code. this will eventually be replaced with a CcH2 analysis.

--- analysis/cryocompressed/cc_discharge.py
+++ analysis/cryocompressed/cc_discharge.py
@@ -1,52 +1,129 @@
 
 
-from analysis.study_tank_geometry import analyse_tank
-from src.insulation.foam_insulations import ConstantFoamInsulation
+from plotting.plot_tank_states import (plot_tank_efficiencies_scatter, plot_single_tank_fill, plot_tank_loads, plot_single_tank_temperatures, plot_single_required_flux, plot_single_tank_loads, plot_density_vs_temperature)
+from plotting.tank_render import plot_tank
+from facades.analysis_facades import (DrainingAnalysisFacade, InitialConditions,
+                                      OperatingEnvelope, TankDimensions, GenericTankDimensions, MissionAnalysisFacade)
+from src.insulation.foam_insulations import ConstantFoamInsulation, VariableFoamInsulation
 from src.materials.materials import Composite
 from src.mission.mission_sections import OutFlow
-from facades.analysis_facades import InitialConditions
+from src.mission.mission import Mission
+from src.thermodynamics.tank_states import InitialState
+from src.tank_design.tank_shapes import WinnefeldTank, CylindricalTankSphericalCaps
+import matplotlib.pyplot as plt
 import numpy as np
+import os
+import time
+import yaml
+from src.mission.mission import Mission
 
 
 def perform_analysis():
 
-    # Define the initial state of the tank
-    pressure = 1.4e5
-    temperature = None
-    fill = 0.97
-    initial_state = InitialConditions(
-        pressure, temperature, fill
-    )
+    # Record the start time
+    start_time = time.time()
 
-    # Define insulation and thermal model
-    thickness = 4e-2
-    insulation = ConstantFoamInsulation.rohacell(thickness)
+    # Get the directory of the current script
+    script_dir = os.path.dirname(__file__)
+    # Construct the path to the YAML file
+    yaml_path = os.path.join(script_dir, 'input.yaml')
 
-    # Define the material of the tank
-    winding_angle = 55
-    material = Composite.carbon(np.radians(winding_angle))
+    # Load configuration from YAML file
+    with open(yaml_path, 'r') as file:
+        config = yaml.safe_load(file)
 
-    # Define fuel flow
-    fuel_flow_phase = "gas"
-    fuel_flow = OutFlow.rompokos_cruise(fuel_flow_phase)
+    # Extract parameters from the YAML file
+    radius = config.get('tank radius', None)
+    insulation_thickness = config.get('insulation thickness', None)
+    VOLUME_MARGIN = config.get('volume margin', None)
+    tank_material_type = config.get('tank material type', None)
+    tank_material_name = config.get('tank material', None)
+    winding_angle_degrees = config.get('winding angle', None)
+    mission_name = config.get('mission', None)
+    fuel_flow = config.get('fuel flow', None)
+    min_pressure = config.get('operating floor pressure', None)
+    min_temperature = config.get('operating floor temperature', None)
+    max_pressure = config.get('operating ceiling pressure', None)
+    initial_pressure = config.get('initial pressure', None)
+    initial_temperature = config.get('initial temperature', None)
+    fill = config.get('target fill', None)
+    head_type = config.get('head type', None)
 
-    # Define the minimum pressure of the tank
-    min_pressure = 1.4e5
+    initial_state = InitialState(initial_pressure, initial_temperature, fill)
 
-    # Define the levels for the contour plot
-    levels = [i / 100 for i in range(82, 101, 2)]
+    winding_angle = np.radians(winding_angle_degrees)
+        # Instantiate the tank material
+    tank_material_class = globals()[tank_material_type]
+    if tank_material_name == 'carbon':
+        tank_material = getattr(tank_material_class, tank_material_name)(winding_angle)
+    else:
+        tank_material = getattr(tank_material_class, tank_material_name)()
 
-    analyse_tank(
-        initial_state,
+    # Define the mission using the string from the input file
+    mission = getattr(Mission, mission_name)(fuel_flow)
+    mission = getattr(Mission, mission_name)(fuel_flow)
+
+    operating_window = OperatingEnvelope(max_pressure, min_pressure=min_pressure, min_temperature=min_temperature)
+
+    # Define required fuel
+    fuel_mass = mission.required_fuel
+    initial_fuel = initial_state.get_hydrogen_properties()
+
+    # Get fuel volume
+    fuel_volume = fuel_mass / initial_fuel.liquid.density
+    
+    # Instantiate the insulation
+    insulation = ConstantFoamInsulation.rohacell(insulation_thickness)
+
+    # Calculate the tank length based on SE or Hemi head configuration
+    # Instantiate the tank dimensions
+    if head_type == 'hemi':
+        length = CylindricalTankSphericalCaps.length_from_radius_and_volume(radius, VOLUME_MARGIN * fuel_volume)
+        tank_dimensions = CylindricalTankSphericalCaps(radius, length, tank_material, initial_pressure)
+    elif head_type == 'se':
+        length = WinnefeldTank.length_from_radius_b_and_volume(radius, VOLUME_MARGIN * fuel_volume, 0.5*radius)
+        tank_dimensions = GenericTankDimensions(radius,length , radius, 0.5*radius)
+    else:
+        raise ValueError(f"Unsupported head type: {head_type}")
+    
+    # Print the parameters
+    print(f"Tank length: {length}")
+    print(f"Tank radius: {radius}")
+    print(f"Fuel volume: {fuel_volume}")
+    print(f"Insulation thickness: {insulation_thickness}")
+    print(f"Initial state: {initial_state}")
+    
+    # perform the mission analysis
+    performance = MissionAnalysisFacade.analyse(
+        tank_dimensions,
+        tank_material,
         insulation,
-        material,
-        min_pressure,
-        fuel_flow.mass_flow,
-        fuel_flow.phase,
-        levels,
-        "twophase_liquid_drain",
-        1
+        mission,
+        initial_state,
+        operating_window
     )
+    
+    # Print the final liquid and gas densities
+    final_liquid_density = performance.tank_states.last_state.hydrogen.liquid.density
+    final_gas_density = performance.tank_states.last_state.hydrogen.gas.density
+    print(f"Final liquid density: {final_liquid_density}")
+    print(f"Final gas density: {final_gas_density}")
+
+    # listify the densities for plotting
+    liquid_densities = [state.hydrogen.liquid.density for state in performance.tank_states.states]
+    gas_densities = [state.hydrogen.gas.density for state in performance.tank_states.states]
+    
+    # Print the time taken
+    print(f"Time taken: {time.time() - start_time}")
+    
+    # Plotting
+    fig_tank_temperatures = plot_single_tank_temperatures(performance.tank_states)
+    fig_tanK_pressures = plot_single_tank_loads(performance.tank_states)
+    fig_req_flux = plot_single_required_flux(performance.tank_states)
+    fig_tank_fill = plot_single_tank_fill(performance.tank_states)
+    fig_density_vs_temperature_gas = plot_density_vs_temperature(performance.tank_states, gas_densities)
+    fig_density_vs_temperature_liquid = plot_density_vs_temperature(performance.tank_states, liquid_densities)
+    plt.show()
 
 
 def main():
