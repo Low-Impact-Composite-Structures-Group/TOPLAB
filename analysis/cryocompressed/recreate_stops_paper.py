bcf5f9623d9d1e6b5c8b9ea811587b4c39a0dca4
Dante Raso <d.raso@tudelft.nl>
2025-02-04 22:54:48 +0100

    add script to recreate results from Stops paper for cryocompressed hydrogen tank analysis
    placeholder until refuel, discharge, and dormancy module are finished

---
+++ analysis/cryocompressed/recreate_stops_paper.py
@@ -0,0 +1,8 @@
+
+"""This script is used to recreate the results from the paper:
+Generalized thermodynamic modeling of hydrogen storage tanks for truck
+application by Laura Stops, Daniel Siebe, Alexander Stary, Johannes Hamacher, Valeryia Sidarava,
+Sebastian Rehfeldt, Harald Klein
+
+The script is used to perform a drive cycle analysis for a cryocompressed hydrogen tank in a truck application.
+"""
