3b49f2bac27255fed69dee3dfae4796d3d9a2e11
Dante Raso <d.raso@tudelft.nl>
2025-02-04 22:56:21 +0100

    setup refuelling scenarios vaguely matching quantities in Stops paper
    values need to be updated and plotting needs to be fixed to match discharge plots

---
+++ analysis/cryocompressed/cc_refuel.py
@@ -0,0 +1,170 @@
+
+
+from plotting.plot_tank_states import plot_tank_efficiencies_scatter, plot_single_tank_fill, plot_tank_loads, plot_single_tank_temperatures, plot_single_required_flux, plot_single_tank_loads, plot_density_vs_temperature, plot_required_flux, plot_thermo_mechanical_loading, plot_tank_fill
+from plotting.tank_render import plot_tank
+from facades.analysis_facades import FillingAnalysisFacade, InitialConditions, OperatingEnvelope, TankDimensions, TargetConditions
+from src.insulation.foam_insulations import ConstantFoamInsulation, VariableFoamInsulation
+from src.materials.materials import Composite
+from src.mission.mission import Mission
+from src.thermodynamics.tank_states import InitialState
+from src.tank_design.tank_shapes import WinnefeldTank, CylindricalTankSphericalCaps
+from src.fluids.hydrogen_retrievers import SinglePhaseRequester, TwoPhaseRequester
+from src.mission.mission import Mission
+from src.mission.mission_sections import InFlow, MissionSection
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import time
+import yaml
+
+
+def perform_analysis():
+
+    # Record the start time
+    start_time = time.time()
+
+    # Get the directory of the current script
+    script_dir = os.path.dirname(__file__)
+    # Construct the path to the YAML file
+    yaml_path = os.path.join(script_dir, 'input.yaml')
+
+    # Load configuration from YAML file
+    with open(yaml_path, 'r') as file:
+        config = yaml.safe_load(file)
+
+    # Extract parameters from the YAML file
+    radius = config.get('tank properties', {}).get('tank radius', None)
+    length = config.get('tank properties', {}).get('tank length', None)
+    insulation_thickness =config.get('tank properties', {}).get('insulation thickness', None)
+    tank_material_type = config.get('tank properties', {}).get('tank material type', None)
+    tank_material_name = config.get('tank properties', {}).get('tank material', None)
+    winding_angle_degrees = config.get('tank properties', {}).get('winding angle', None)
+    head_type = config.get('tank properties', {}).get('head type', None)
+    min_pressure = config.get('refueling', {}).get('min pressure', None)
+    min_temperature = config.get('refueling', {}).get('min temperature', None)
+    max_pressure = config.get('refueling', {}).get('max pressure', None)
+    initial_pressure = config.get('refueling', {}).get('initial pressure', None)
+    initial_temperature = config.get('refueling', {}).get('initial temperature', None)
+    initial_fill = config.get('refueling', {}).get('initial fill', None)
+    mass_flow = config.get('refueling', {}).get('mass flow rate', None)
+    duration = config.get('refueling', {}).get('duration', None)
+    altitude = config.get('refueling', {}).get('altitude', None)
+    mach_number = config.get('refueling', {}).get('mach number', None)
+    target_fill = config.get('refueling', {}).get('target fill', None)
+    target_mass = config.get('refueling', {}).get('target mass', None) 
+    
+    # Check for None values and print them
+    parameters = {
+        'tank radius': radius,
+        'insulation thickness': insulation_thickness,
+        'tank material type': tank_material_type,
+        'tank material': tank_material_name,
+        'winding angle': winding_angle_degrees,
+        'head type': head_type,
+        'min pressure': min_pressure,
+        'min temperature': min_temperature,
+        'max pressure': max_pressure,
+        'initial pressure': initial_pressure,
+        'initial temperature': initial_temperature,
+        'initial fill': initial_fill,
+        'mass flow rate': mass_flow,
+        'duration': duration,
+        'altitude': altitude,
+        'mach number': mach_number,
+        'target fill': target_fill,
+        'target mass': target_mass
+    }
+
+    for param, value in parameters.items():
+        if value is None:
+            print(f"Warning: {param} is None")
+    
+    initial_conditions = InitialConditions(
+        initial_pressure, initial_temperature, initial_fill
+    )
+
+    winding_angle = np.radians(winding_angle_degrees)
+    # Instantiate the tank material
+    tank_material_class = globals()[tank_material_type]
+    if tank_material_name == 'carbon':
+        tank_material = getattr(tank_material_class, tank_material_name)(winding_angle)
+    else:
+        tank_material = getattr(tank_material_class, tank_material_name)()
+    
+    # Define the fuel tank
+    tank = CylindricalTankSphericalCaps(radius, length, tank_material, initial_pressure)
+    tank_body = tank.body_length
+    tank_dimensions = TankDimensions(radius, tank_body)
+
+    # Define the target conditions
+    target_conditions = TargetConditions(target_mass, target_fill)
+    
+    # Define operating window
+    operating_window = OperatingEnvelope(max_pressure, min_pressure, min_temperature)
+    
+    # Instantiate the insulation
+    insulation = ConstantFoamInsulation.rohacell(insulation_thickness)
+
+    mission_section = MissionSection(
+            duration,
+            [
+                InFlow(
+                    mass_flow,
+                    SinglePhaseRequester().get_hydrogen_properties(
+                        initial_pressure, initial_temperature
+                    )
+                )
+            ],
+            altitude,
+            mach_number
+        )
+    mission = Mission([mission_section])
+    
+    tank_performance = FillingAnalysisFacade.analyse(
+        tank_dimensions,
+        tank_material,
+        insulation,
+        mission,
+        initial_conditions,
+        operating_window,
+        target_conditions
+    )
+
+    # Print some parameters
+    print(f"Tank length: {length}")
+    print(f"Tank radius: {radius}")
+    print(f"Insulation thickness: {insulation_thickness}")
+    
+    tank_states = tank_performance.tank_states
+
+    y1ticks = None
+    y2ticks = None
+    xticks = None
+    plot_thermo_mechanical_loading(
+        tank_states,
+        xticks,
+        y1ticks,
+        y2ticks
+    )
+    y1ticks = None
+    y2ticks = None
+    plot_tank_fill(
+        tank_states,
+        xticks,
+        y1ticks,
+        y2ticks
+    )
+    plt.show()
+
+
+
+
+def main():
+    perform_analysis()
+
+
+if __name__ == "__main__":
+    main()
+
+
+# End
