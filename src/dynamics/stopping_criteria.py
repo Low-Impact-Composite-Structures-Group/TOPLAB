138760900c0c15dd766aa7a2d6c7b5e5d24ceb19
Dante Raso <d.raso@tudelft.nl>
2025-02-10 20:34:40 +0100

    create new stopping criterion based on specified hydrogen density value

--- src/dynamics/stopping_criteria.py
+++ src/dynamics/stopping_criteria.py
@@ -10,6 +10,7 @@ class TargetState(Protocol):
     min_temperature: float
     fill: float
     mass: float
+    density: float = None
 
 
 class FuelTankState(Protocol):
@@ -97,6 +98,19 @@ class MaxPressureReached(StoppingCriterion):
     ) -> bool:
         return fuel_tank_state.pressure >= target_state.max_pressure
 
+class TargetDensityReached(StoppingCriterion):
+
+    def is_met(
+        self, fuel_tank_state: FuelTankState, target_state: TargetState
+    ) -> bool:
+        phase = fuel_tank_state.hydrogen.phase
+        if phase == 'twophase':
+            density = fuel_tank_state.hydrogen.two_phase.density
+        elif phase in ["gas", "supercritical"]:
+            density = fuel_tank_state.hydrogen.gas.density
+        elif phase in ["liquid", "supercritical_liquid"]:
+            density = fuel_tank_state.hydrogen.liquid.density
+        return density >= target_state.density
 
 def main():
     pass
