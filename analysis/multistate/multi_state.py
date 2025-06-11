157468cc083dfc64084a07ee5c849c4ff2ffbd29
Dante Raso <d.raso@tudelft.nl>
2025-06-11 15:18:31 +0200

    feat: enhance perform_analysis to support dual tank analysis and plotting. just setting up scaffolding; none of the values used are accurate

--- analysis/multistate/multi_state.py
+++ analysis/multistate/multi_state.py
@@ -1,10 +1,13 @@
 import matplotlib.pyplot as plt
-from facades.analysis_facades import MissionAnalysisFacade, OperatingEnvelope, TankDimensions
+from plotting.plot_tank_states import plot_single_tank_fill, plot_tank_loads, plot_single_tank_temperatures, plot_single_required_flux, plot_single_tank_loads, plot_density_vs_temperature
+from facades.analysis_facades import MissionAnalysisFacade, OperatingEnvelope, TankDimensions, FillingAnalysisFacade, TargetConditions, InitialConditions, DualTankAnalysisFacade
 from src.insulation.foam_insulations import ConstantFoamInsulation
 from src.materials.materials import Composite
 from src.mission.mission import Mission
+from src.mission.mission_sections import InFlow, MissionSection
 from src.thermodynamics.tank_states import InitialState
 from src.tank_design.tank_shapes import SphericalTank
+from src.fluids.hydrogen_retrievers import SinglePhaseRequester, TwoPhaseRequester
 import numpy as np
 import os
 import yaml
@@ -39,15 +42,16 @@ def perform_analysis():
     t_min_2 = config.get('tank 2', {}).get('min temperature', None)
     ambient_heat_load_2 = config.get('tank 2', {}).get('ambient heat load', None)
 
-    # instantiate the tanks
+    # instantiate the Tank and TankDimensions object
     tank_material = Composite.carbon(np.radians(55))
-
     tank_1 = SphericalTank(radius_1, tank_material, p_init_1)
     tank_2 = SphericalTank(radius_2, tank_material, p_init_2)
+    tank_dimensions_1 = TankDimensions(radius_1, 0.0)
+    tank_dimensions_2 = TankDimensions(radius_2, 0.0)
 
     # Mission and material
-    fuel_flow = "gas"
-    mission = Mission.regional(fuel_flow)
+    mission_1 = Mission.triathlon()
+    mission_2 = Mission.triathlon()
     tank_material = Composite.carbon(np.radians(55))
     # TODO: remove dummy insulation; not used since we are applying constant heat load
     insulation_thickness = 0.001  # [m]
@@ -59,16 +63,40 @@ def perform_analysis():
 
     operating_window_1 = OperatingEnvelope(p_max_1, p_min_1, t_min_1)
     operating_window_2 = OperatingEnvelope(p_max_2, p_min_2, t_min_2)
+    # print(f"Initial State 1: {initial_state_1}")
+    # print(f"Initial State 2: {initial_state_2}")
+    # print(f"Operating Window 1: {operating_window_1}")
+    # print(f"Operating Window 2: {operating_window_2}")
 
-    # print(f"Tank 1: {tank_1}")
-    # print(f"Tank 2: {tank_2}")
-
-    print(f"Initial State 1: {initial_state_1}")
-    print(f"Initial State 2: {initial_state_2}")
-    print(f"Operating Window 1: {operating_window_1}")
-    print(f"Operating Window 2: {operating_window_2}")
     # Calculate required fuel and split between tanks
-    # fuel_mass = mission.required_fuel
-    # initial_fuel = initial_state_1.get_hydrogen_properties()
-    # fuel_volume = fuel_mass / initial_fuel.liquid.density
-    # tank_volume = 1.2 * fuel_volume / 2  # Split between 2 tanks
+    fuel_mass_1 = mission_1.required_fuel
+    fuel_mass_2 = mission_2.required_fuel
+    print(f"Required fuel mass for tank 1: {fuel_mass_1} kg")
+    print(f"Required fuel mass for tank 2: {fuel_mass_2} kg")
+
+    tank_performance = DualTankAnalysisFacade.analyse(
+        tank_dimensions_1,
+        tank_dimensions_2,
+        tank_material,
+        insulation,
+        mission_1,
+        ambient_heat_load_1,
+        InitialConditions(p_init_1, t_init_1, fill_1),
+        InitialConditions(p_init_2, t_init_2, fill_2),
+        operating_window_1,
+        operating_window_2,
+        TargetConditions(fuel_mass_2, 0.0)
+    )
+    tank_performance_1 = tank_performance.tank_states[0]
+    tank_performance_2 = tank_performance.tank_states[1]
+
+    fig_tank_fill_1 = plot_single_tank_fill(tank_performance_1)
+    fig_tank_fill_2 = plot_single_tank_fill(tank_performance_2)
+    fig_tank_temperatures_1 = plot_single_tank_temperatures(tank_performance_1)
+    fig_tank_temperatures_2 = plot_single_tank_temperatures(tank_performance_2)
+    fig_tank_pressures_1 = plot_single_tank_loads(tank_performance_1)
+    fig_tank_pressures_2 = plot_single_tank_loads(tank_performance_2)
+
+    plt.show()
+
+
