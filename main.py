cd9844a964863153265008bb99eb9518c3329708
Dante Raso <d.raso@tudelft.nl>
2025-08-04 14:54:43 +0200

    update import statements for analysis modules

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-from analysis.multistate.multi_state import perform_analysis
+# from analysis.multistate.in_out_model_two_phase import perform_analysis
+# from analysis.multistate.in_out_model_single_phase import perform_analysis
+# from analysis.multistate.multi_state_single_phase import perform_analysis
+from analysis.multistate.hpgh2_to_cch2 import perform_analysis
+# from analysis.cryocompressed.cc_discharge import perform_analysis
+
 
 def main():
     perform_analysis()
