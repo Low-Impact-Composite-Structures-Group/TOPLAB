74c1a0d6d246126012ca3355145632316bfc173b
Dante Raso <d.raso@tudelft.nl>
2025-09-16 11:00:51 +0200

    revert to pre-branching. for the time being we will interface with analysis scripts directly instead of importing the perform_analysis function in the main function

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-# from analysis.multistate.in_out_model_two_phase import perform_analysis
-# from analysis.multistate.in_out_model_single_phase import perform_analysis
-# from analysis.multistate.multi_state_single_phase import perform_analysis
-from analysis.multistate.hpgh2_to_cch2 import perform_analysis
-# from analysis.cryocompressed.cc_discharge import perform_analysis
+
+
+from analysis.study_missions_gas import perform_analysis
 
 
 def main():
     perform_analysis()
 
+
 if __name__ == "__main__":
     main()
+
+
+# End
